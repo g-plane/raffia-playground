08ed32a5187fbfc56be01d084afce540618ea4d5
Pig Fang <g-plane@hotmail.com>
2022-08-26 17:09:39 +0800

    polish tree view

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -37,19 +37,27 @@ const Node: Component<{ node: any }> = (props) => {
                 when={Array.isArray(value)}
                 fallback={
                   <>
-                    <span class="ml-4 mr-1">{key}:</span>
+                    <span class="ml-4 mr-1">
+                      <code class="text-purple-800">{key}</code>:
+                    </span>
                     <Switch>
                       <Match when={typeof value === 'object' && value != null}>
                         <Node node={value} />
                       </Match>
-                      <Match
-                        when={
-                          typeof value === 'string' ||
-                          typeof value === 'number' ||
-                          typeof value === 'boolean'
-                        }
-                      >
-                        <span>{value as string | number | boolean}</span>
+                      <Match when={typeof value === 'string'}>
+                        <code class="text-rose-800 ml-1">
+                          {value as string}
+                        </code>
+                      </Match>
+                      <Match when={typeof value === 'number'}>
+                        <code class="text-yellow-700 ml-1">
+                          {value as number}
+                        </code>
+                      </Match>
+                      <Match when={typeof value === 'boolean'}>
+                        <code class="text-sky-700 ml-1">
+                          {(value as boolean).toString()}
+                        </code>
                       </Match>
                     </Switch>
                   </>
@@ -72,8 +80,8 @@ const Node: Component<{ node: any }> = (props) => {
                       <i class="fa-solid fa-caret-down"></i>
                     </Show>
                   </span>
-                  {key}:
-                  <span class="ml-3">
+                  <code class="text-purple-800">{key}</code>:
+                  <span class="ml-3 text-sm">
                     <Switch
                       fallback={
                         <span>[ {(value as unknown[]).length} elements ]</span>
