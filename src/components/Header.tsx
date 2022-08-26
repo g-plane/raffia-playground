6030ec75ada364ac923cffa423754f2fd3f0fd44
Pig Fang <g-plane@hotmail.com>
2022-08-26 17:17:34 +0800

    tweak style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js'
 
 const Header: Component = () => (
-  <header class="h-14 p-3 grid grid-cols-2 bg-sky-50">
+  <header class="h-14 p-3 grid grid-cols-2 bg-sky-50 border-b-width-2px border-b-sky-200">
     <div class="text-lg font-semibold">Raffia Playground</div>
   </header>
 )
