f7e408d15759721ef2da8c4805a16867ab95c708
Pig Fang <g-plane@hotmail.com>
2022-09-06 08:26:42 +0800

    refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,11 @@ import {
   Show,
   onCleanup,
   createEffect,
-  useContext,
 } from 'solid-js'
-import { globalOptionsContext } from '../state'
+import OptionsDialog from './OptionsDialog'
 
 const Header: Component = () => {
   const [showOptions, setShowOptions] = createSignal(false)
-  const [globalOptions, setGlobalOptions] = useContext(globalOptionsContext)
 
   let el: HTMLDivElement | undefined
   createEffect(() => {
@@ -40,67 +38,7 @@ const Header: Component = () => {
             <i class="fa-solid fa-gear mr-1"></i>Options
           </button>
           <Show when={showOptions()}>
-            <section
-              ref={el}
-              class="border-emerald-600 border-width-1px rounded-sm p-3 w-60 bg-white fixed text-base text-stone-900 grid grid-cols-1 gap-y-2"
-            >
-              <p class="flex justify-between">
-                <span>Syntax</span>
-                <select
-                  value={globalOptions.syntax}
-                  onInput={(e) =>
-                    setGlobalOptions('syntax', e.currentTarget.value)
-                  }
-                >
-                  <option value="css">CSS</option>
-                  <option value="scss">SCSS</option>
-                  <option value="sass">Sass</option>
-                  <option value="less">Less</option>
-                </select>
-              </p>
-              <p class="flex justify-between">
-                <span>View</span>
-                <span class="grid grid-cols-2 gap-x-1">
-                  <label>
-                    <input
-                      type="radio"
-                      class="mr-0.5"
-                      name="view"
-                      value="tree"
-                      checked={globalOptions.view === 'tree'}
-                      onInput={() => setGlobalOptions('view', 'tree')}
-                    />
-                    Tree
-                  </label>
-                  <label>
-                    <input
-                      type="radio"
-                      class="mr-0.5"
-                      name="view"
-                      value="json"
-                      checked={globalOptions.view === 'json'}
-                      onInput={() => setGlobalOptions('view', 'json')}
-                    />
-                    JSON
-                  </label>
-                </span>
-              </p>
-              <p>
-                <label class="flex justify-between items-center">
-                  <span>Highlight On Hover</span>
-                  <input
-                    type="checkbox"
-                    checked={globalOptions.highlightOnHover}
-                    onInput={(e) =>
-                      setGlobalOptions(
-                        'highlightOnHover',
-                        e.currentTarget.checked
-                      )
-                    }
-                  />
-                </label>
-              </p>
-            </section>
+            <OptionsDialog ref={el} />
           </Show>
         </div>
 
